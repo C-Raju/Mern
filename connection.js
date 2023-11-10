abdea5383ee9ad6e0c70c1816a4578192744d145
C-Raju <126947587+C-Raju@users.noreply.github.com>
2023-11-10 11:31:50 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 
 const connection = async () => {
   const client = MongoClient.connect(
-    "mongodb+srv://chandrashekharraju306:29@shekhar@cluster0.7a0l0dg.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://chandrashekharraju306:29@shekhar@cluster0.7a0l0dg.mongodb.net/blog?retryWrites=true&w=majority"
   );
   const db = (await client).db("blog");
   return { db };
